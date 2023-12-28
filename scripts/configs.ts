ef3fe221020de2a5042d845c7840815826c3f6dd
aoki <aokigames12@gmail.com>
2023-12-28 21:04:44 +0900

    修正

--- scripts/configs.ts
+++ scripts/configs.ts
@@ -1,11 +1,7 @@
 import { world } from "@minecraft/server";
 
 export const config = {
-  mainScoreName: "mainScore",
-  moneyScoreName: "moke",
-  prefix: "!",
-  commission: 0.9,
-  zombieSpawnPos: [
+  buttonPos: [
     { x: -96, y: -59, z: -1 },
     { x: -94, y: -59, z: 14 },
     { x: -115, y: -60, z: 8 },
--- scripts/index.ts
+++ scripts/index.ts
@@ -2,38 +2,75 @@ import { Container, ItemStack, system, world } from "@minecraft/server";
 import { ModalFormData, ActionFormData } from "@minecraft/server-ui";
 import { config } from "./configs";
 
-world.beforeEvents.itemUse.subscribe((ev) => {
-  if (world.getDynamicProperty("main") == true) {
-    if (!ev.source.hasTag("Builder") || ev.source.hasTag("Visitor")) ev.cancel = true;
+const overworld = world.getDimension("overworld");
+
+system.runInterval(() => {
+  for (const pos of config.buttonPos) {
+    let i = 0;
+    const player = overworld.getPlayers({ location: { x: pos.x, y: pos.y, z: pos.z } });
+    if (!player) return;
+    player.forEach((p) => {
+      const button = overworld.getEntities({ location: { x: pos.x, y: pos.y, z: pos.z }, families: [""] })[0];
+      const buttonProperty = {};
+      const buttonCount = Number(p.getDynamicProperty("buttonCount")) ?? 0;
+      p.sendMessage(``);
+      p.setDynamicProperty("buttonCount", buttonCount + 1);
+      switch (i) {
+        case 0: {
+          p.setDynamicProperty("multi", 1);
+          break;
+        }
+        case 1: {
+          p.setDynamicProperty("reb", 1);
+          break;
+        }
+        case 2: {
+          p.setDynamicProperty("", 1);
+          break;
+        }
+        case 3: {
+          p.setDynamicProperty("", 1);
+          break;
+        }
+        case 4: {
+          p.setDynamicProperty("", 1);
+          break;
+        }
+      }
+    });
+    i++;
   }
+}, 10);
+
+system.runInterval(() => {
+  for (const player of world.getAllPlayers()) {
+    const cash = Number(player.getDynamicProperty("cash")) ?? 0;
+    player.setDynamicProperty("cash", cash + 1);
+  }
+}, 20);
+
+system.runInterval(() => {
+  for (const player of world.getAllPlayers()) {
+    player.setDynamicProperty("");
+  }
+}, 20);
+
+world.afterEvents.playerJoin.subscribe((ev) => {
+  const { playerName } = ev;
+  const player = overworld.getPlayers({ name: playerName })[0];
+  if (!player.getDynamicProperty("join")) player.setDynamicProperty("join", getTime());
+  const joinCount = Number(player.getDynamicProperty("joinCount")) ?? 0;
+  player.setDynamicProperty("joinCount", joinCount + 1);
+  console.warn(getTime());
 });
 
-world.beforeEvents.playerBreakBlock.subscribe((ev) => {
-  if (world.getDynamicProperty("main") == true) {
-    if (!ev.player.hasTag("Builder") || ev.player.hasTag("Visitor")) ev.cancel = true;
-  }
-});
+function getTime() {
+  const Year = new Date().getFullYear();
+  const Month = new Date().getMonth() + 1;
+  const Date_ = new Date().getDate();
+  const Hour = new Date().getHours();
+  const Min = new Date().getMinutes();
+  const Sec = new Date().getSeconds();
 
-world.beforeEvents.playerPlaceBlock.subscribe((ev) => {
-  if (world.getDynamicProperty("main") == true) {
-    if (!ev.player.hasTag("Builder") || ev.player.hasTag("Visitor")) ev.cancel = true;
-  }
-});
-
-world.beforeEvents.playerInteractWithBlock.subscribe((ev) => {
-  if (world.getDynamicProperty("main") == true) {
-    if (!ev.player.hasTag("Builder") || ev.player.hasTag("Visitor")) ev.cancel = true;
-  }
-});
-
-world.beforeEvents.playerInteractWithEntity.subscribe((ev) => {
-  if (world.getDynamicProperty("main") == true) {
-    if (!ev.player.hasTag("Builder") || ev.player.hasTag("Visitor")) ev.cancel = true;
-  }
-});
-
-world.beforeEvents.itemUseOn.subscribe((ev) => {
-  if (world.getDynamicProperty("main") == true) {
-    if (!ev.source.hasTag("Builder") || ev.source.hasTag("Visitor")) ev.cancel = true;
-  }
-});
+  return Year + "年" + Month + "月" + Date_ + "日" + Hour + ":" + Min + ":" + Sec;
+}
